7bce34533b2540b41c1f0ca22b00a9cf9f9ff6f3
Basil Ugo <basilugo2@gmail.com>
2024-10-07 10:27:02 +0100

    update styles

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const SearchBar = () => {
 	return (
-		<div className="bg-white rounded-full shadow-md px-4 py-2 flex items-center">
+		<div className="bg-white rounded-lg shadow-md px-4 py-2 flex items-center w-2/5">
+			<span>icon </span>
 			<input
 				type="text"
 				placeholder="Search"
-				className="border-none focus:outline-none w-full"
+				className="border-none focus:outline-none w-full py-2"
 			/>
 		</div>
 	);
