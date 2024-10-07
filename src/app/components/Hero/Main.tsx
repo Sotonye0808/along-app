4b268ccafbf74c7513989d10fb188519c00ee330
Basil Ugo <basilugo2@gmail.com>
2024-10-07 13:51:29 +0100

    left part of hero page / image part; add images and src for carousel

---
+++ src/app/components/Hero/Main.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from "react";
+
+const Main = () => {
+	return (
+		<div className="flex-1 flex flex-col items-center justify-center p-12">
+			<h1 className="text-2xl text-gray-600 text-center font-semibold">
+				Decrease the time spent on locating the next stop or destination with
+				our search feature.
+			</h1>
+		</div>
+	);
+};
+
+export default Main;
