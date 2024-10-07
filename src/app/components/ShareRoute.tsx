887af5859b967a8ec7a9fb45b26ba9a9fe4a6189
Basil Ugo <basilugo2@gmail.com>
2024-10-07 10:27:09 +0100

    update styles

--- src/app/components/ShareRoute.tsx
+++ src/app/components/ShareRoute.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const ShareRoute = () => {
 	return (
-		<div className="p-4 bg-white rounded-md shadow-md flex items-center justify-between">
+		<div className="p-2 bg-white rounded-md shadow-md flex items-center justify-between w-3/5 mr-4">
 			<div className="flex items-center">
-				<div className="w-10 h-10 rounded-full bg-gray-300 mr-3"></div>
+				<div className="w-9 h-8 rounded-full bg-gray-300 ml-2"></div>
 				<input
 					type="text"
 					placeholder="Share a route..."
-					className="border border-gray-300 rounded-full py-2 px-4 w-full focus:outline-none focus:border-green-500"
+					className="rounded-full py-2 px-4 w-full focus:outline-none focus:border-green-500"
 				/>
 			</div>
-			<button className="ml-4 bg-green-500 text-white px-4 py-2 rounded-full">
+			<button className="ml-4 bg-green-700 text-white px-4 py-2 rounded-md">
 				Post
 			</button>
 		</div>
--- src/app/components/SuggestionsPanel.tsx
+++ src/app/components/SuggestionsPanel.tsx
@@ -9,12 +9,15 @@ const SuggestionsPanel = () => {
 	];
 
 	return (
-		<div className="p-4 bg-white rounded-md shadow-md w-full">
-			<h2 className="font-semibold mb-4">Suggestions</h2>
+		<div className="p-4 bg-transparent rounded-md w-full">
+			<h2 className="font-semibold mb-4 text-2xl">Suggestions</h2>
 			<ul className="space-y-4">
 				{suggestions.map((item) => (
-					<li key={item.name} className="flex justify-between text-gray-700">
-						<span>{item.name}</span>
+					<li
+						key={item.name}
+						className="flex justify-between text-gray-700 flex-col"
+					>
+						<span className="font-bold text-gray-950">{item.name}</span>
 						<span className="text-gray-500">{item.posts}</span>
 					</li>
 				))}
