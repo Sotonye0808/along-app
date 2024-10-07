aace577fcb109b5bf142d19995006f053554da10
Basil Ugo <basilugo2@gmail.com>
2024-10-07 10:10:40 +0100

    update styles for profile img, indentation for posts/blog post

--- src/app/components/MainPanel.tsx
+++ src/app/components/MainPanel.tsx
@@ -6,38 +6,40 @@ const MainPanel = () => {
 			{/* sample post */}
 			<div className="p-4 bg-white rounded-md shadow-md">
 				<div className="flex items-center mb-4">
-					<div className="w-12 h-12 rounded-full bg-gray-300 mr-4"></div>
-					<div>
-						<div className="font-semibold">Cartoon Network</div>
-						<div className="text-gray-500 text-sm">3h</div>
+					<div className="w-8 h-8 rounded-full bg-gray-300 mr-4"></div>
+					<div className="flex items-center">
+						<div className="font-semibold pr-4">Cartoon Network</div>
+						<span className="text-gray-500 text-sm">3h</span>
 					</div>
 				</div>
-				<p className="text-gray-700 mb-4">
-					Lorem ipsum dolor sit amet consectetur. Adipiscing donec turpis eu
-					mattis nisl a placerat.
-				</p>
-				<div className="mb-4">
-					<img
-						src="https://via.placeholder.com/400x200"
-						alt="Post content"
-						className="rounded-md"
-					/>
-				</div>
-				<div className="flex space-x-8 text-gray-500">
-					<div className="flex items-center space-x-2">
-						<span>
-							👍
-							{/* i don't know what ui kini we're using */}
-						</span>
-						<span>417</span>
+				<div className="pl-12">
+					<p className="text-gray-700 mb-4">
+						Lorem ipsum dolor sit amet consectetur. Adipiscing donec turpis eu
+						mattis nisl a placerat.
+					</p>
+					<div className="mb-4">
+						<img
+							src="https://via.placeholder.com/400x200"
+							alt="Post content"
+							className="rounded-md"
+						/>
 					</div>
-					<div className="flex items-center space-x-2">
-						<span>💬</span>
-						<span>417</span>
-					</div>
-					<div className="flex items-center space-x-2">
-						<span>🔗</span>
-						<span>Share</span>
+					<div className="flex space-x-8 text-gray-500">
+						<div className="flex items-center space-x-2">
+							<span>
+								👍
+								{/* i don't know what ui kini we're using */}
+							</span>
+							<span>417</span>
+						</div>
+						<div className="flex items-center space-x-2">
+							<span>💬</span>
+							<span>417</span>
+						</div>
+						<div className="flex items-center space-x-2">
+							<span>🔗</span>
+							<span>Share</span>
+						</div>
 					</div>
 				</div>
 			</div>
