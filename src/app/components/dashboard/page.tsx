703a20f3ef12424cd118aeda49e9c531920043ef
Basil Ugo <basilugo2@gmail.com>
2025-01-18 23:20:15 +0100

    add dashboard components

---
+++ src/app/components/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShareRoute from "./ShareRoute";
+import SearchBar from "./SearchBar";
+import Navbar from "./Navbar";
+import MainPanel from "./MainPanel";
+import SuggestionsPanel from "./SuggestionsPanel";
+
+const Dashboard = () => {
+	return <p>yes</p>;
+};
+
+export default Dashboard;
